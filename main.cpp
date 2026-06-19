2c0f36b50d50fa28085a4a884bf46692653ad502
Rio Meidi Ataurrahman <riomeidi29@gmail.com>
2026-06-19 11:38:59 +0700

    Docs: Update

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using namespace std;
  * @param tutup     Jumlah kurung tutup ')' yang sudah dipasang saat ini.
  * @param N         Jumlah maksimal pasang kurung.
  */
-void generateParenthesis(vector<string> &hasil, string &saat_ini, int buka, int tutup, int jumlahPasang)
+void generateParenthesis(vector<string> &hasil, string &saat_ini, int symbolBuka, int symbolTutup, int jumlahPasang)
 {
     // KONDISI BERHENTI (Base Case)
     // Jika panjang string sudah mencapai 2 * N, kombinasi selesai dan valid.
@@ -24,12 +24,12 @@ void generateParenthesis(vector<string> &hasil, string &saat_ini, int buka, int 
 
     // ATURAN KURUNG BUKA
     // Kita bisa terus menambah kurung buka selama jumlahnya belum mencapai N.
-    if (buka < jumlahPasang)
+    if (symbolBuka < jumlahPasang)
     {
         saat_ini.push_back('('); // Lakukan aksi: pasang kurung buka
 
         // Eksplorasi jalur ini lebih dalam
-        generateParenthesis(hasil, saat_ini, buka + 1, tutup, jumlahPasang);
+        generateParenthesis(hasil, saat_ini, symbolBuka + 1, symbolTutup, jumlahPasang);
 
         saat_ini.pop_back(); // BACKTRACKING: cabut kembali kurung buka untuk coba kombinasi lain
     }
@@ -37,12 +37,12 @@ void generateParenthesis(vector<string> &hasil, string &saat_ini, int buka, int 
     // ATURAN KURUNG TUTUP
     // Kita HANYA boleh menambah kurung tutup jika jumlahnya lebih sedikit dari kurung buka.
     // Ini mencegah string invalid seperti "())" di tengah jalan.
-    if (tutup < buka)
+    if (symbolTutup < symbolBuka)
     {
         saat_ini.push_back(')'); // Lakukan aksi: pasang kurung tutup
 
         // Eksplorasi jalur ini lebih dalam
-        generateParenthesis(hasil, saat_ini, buka, tutup + 1, jumlahPasang);
+        generateParenthesis(hasil, saat_ini, symbolBuka, symbolTutup + 1, jumlahPasang);
 
         saat_ini.pop_back(); // BACKTRACKING: cabut kembali kurung tutup untuk coba kombinasi lain
     }
@@ -50,18 +50,18 @@ void generateParenthesis(vector<string> &hasil, string &saat_ini, int buka, int 
 
 int main()
 {
-    int N;
+    int jumlahPasang;
     cout << "<<= Pengecekan Tanda Kurung '()' Kurung Valid =>>" << endl;
     cout << "Masukkan jumlah pasang kurung (N): ";
-    cin >> N;
+    cin >> jumlahPasang;
 
     vector<string> hasil;
     string saat_ini = "";
 
     // Panggil fungsi rekursif dengan nilai awal buka=0 dan tutup=0
-    generateParenthesis(hasil, saat_ini, 0, 0, N);
+    generateParenthesis(hasil, saat_ini, 0, 0, jumlahPasang);
 
-    cout << "\nHasil kombinasi untuk N = " << N << " (" << hasil.size() << " kombinasi):" << endl;
+    cout << "\nHasil kombinasi untuk N = " << jumlahPasang << " (" << hasil.size() << " kombinasi):" << endl;
     for (int i = 0; i < hasil.size(); i++)
     {
         cout << i + 1 << ". " << hasil[i] << endl;
